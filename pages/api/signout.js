cfa705b3dc5f98a43d469014b6ed2c56a3cfabbe
samuel <adejinmisamuel1@gmail.com>
2023-05-24 20:04:45 +0100

    yes

--- pages/api/signout.js
+++ pages/api/signout.js
@@ -3,7 +3,7 @@ import { deleteCookie } from 'cookies-next';
 
 export default async function handler(req, res) {
     if (req.method==="POST"){
-        deleteCookie("Auth", { req, res, path: '/', domain: 'localhost' })   
+        deleteCookie("Auth", { req, res, path: '/', domain: process.env.NEXT_PUBLIC_DOMAIN })   
         return res.status(200).json({signedOut:true}) 
     }
     return res.status(401)
